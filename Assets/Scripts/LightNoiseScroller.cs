8b1eb39da879fab09a14537e546491befc13653a
craftweak <craftweak@gmail.com>
2017-12-21 02:16:32 +0800

    add Ambient Noise moving

---
+++ Assets/Scripts/LightNoiseScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(VolumetricLight))]
+public class LightNoiseScroller : MonoBehaviour {
+
+[SerializeField]
+        float _speed;
+
+        public float speed {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+            GetComponent<VolumetricLight>().NoiseVelocity.y +=  _speed * Time.deltaTime;
+	}
+}
